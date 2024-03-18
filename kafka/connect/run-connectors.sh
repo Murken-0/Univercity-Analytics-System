c4ce0b7649ceae9725aa5420d6091b3d1723a657
Murken-0 <muravievvlad.2002@gmail.com>
2024-03-19 02:11:44 +0300

    neo4j (without date) + refactoring

---
+++ kafka/connect/run-connectors.sh
@@ -0,0 +1,17 @@
+#postgres
+curl -X POST -H "Content-Type: application/json" --data @connector-config/postgres-source.json http://localhost:8083/connectors
+
+#elastic
+curl -X POST -H "Content-Type: application/json" --data @connector-config/elasticsearch-sink.json http://localhost:8083/connectors
+
+#elastic-logs
+curl -X POST -H "Content-Type: application/json" --data @connector-config/elasticsearch-sink-all.json http://localhost:8083/connectors
+
+#neo4j
+curl -X POST -H "Content-Type: application/json" --data @connector-config/neo4j-sink.json http://localhost:8083/connectors
+
+#redis
+curl -X POST -H "Content-Type: application/json" --data @connector-config/redis-sink.json http://localhost:8083/connectors
+
+#mongo
+curl -X POST -H "Content-Type: application/json" --data @connector-config/mongo-sink.json http://localhost:8083/connectors
--- kafka/connect/run.sh
+++ kafka/connect/run.sh
@@ -17,4 +17,12 @@ confluent-hub install neo4j/kafka-connect-neo4j:latest \
 cp /etc/connect/kafka-connect-redis-assembly-6.0.3.jar $COMPONENT_DIR/kafka-connect-redis-assembly-6.0.3.jar
 cp /etc/connect/mongo-cdc-connector.jar $COMPONENT_DIR/mongo-cdc-connector.jar
 
-/etc/confluent/docker/run
+/etc/confluent/docker/run & 
+echo "Waiting for Kafka Connect to start listening on kafka-connect ⏳"
+while [ $$(curl -s -o /dev/null -w %{http_code} http://connect:8083/connectors) -eq 000 ] ; do 
+  echo -e $$(date) " Kafka Connect listener HTTP state: " $$(curl -s -o /dev/null -w %{http_code} http://kafka-connect:8083/connectors) " (waiting for 200)"
+  sleep 5 
+done
+nc -vz connect 8083
+/run-connectors.sh 
+sleep infinity
