b8023f8e96b21cf45ffb74bc1e487a67b2a39d34
Murken-0 <muravievvlad.2002@gmail.com>
2024-03-21 19:38:36 +0300

    mongo

--- kafka/connect/run.sh
+++ kafka/connect/run.sh
@@ -19,10 +19,10 @@ cp /etc/connect/mongo-cdc-connector.jar $COMPONENT_DIR/mongo-cdc-connector.jar
 
 /etc/confluent/docker/run & 
 echo "Waiting for Kafka Connect to start listening on kafka-connect ⏳"
-while [ $$(curl -s -o /dev/null -w %{http_code} http://connect:8083/connectors) -eq 000 ] ; do 
-  echo -e $$(date) " Kafka Connect listener HTTP state: " $$(curl -s -o /dev/null -w %{http_code} http://kafka-connect:8083/connectors) " (waiting for 200)"
+while [ $(curl -s -o /dev/null -w %{http_code} http://connect:8083/connectors) -eq 000 ] ; do 
+  echo -e $(date) " Kafka Connect listener HTTP state: " $(curl -s -o /dev/null -w %{http_code} http://kafka-connect:8083/connectors) " (waiting for 200)"
   sleep 5 
 done
 nc -vz connect 8083
-/run-connectors.sh 
+./run-connectors.sh 
 sleep infinity
